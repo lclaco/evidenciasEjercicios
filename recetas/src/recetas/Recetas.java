89eea968575b68fcd69caecccc1ddcb2b22d86c7
lclaco <leonelclavijo@gmail.com>
2022-03-23 18:36:42 -0300

    Add files via upload

---
+++ recetas/src/recetas/Recetas.java
@@ -0,0 +1,23 @@
+package recetas;
+
+public class Recetas {
+
+	public static void main(String[] args) {
+		
+		String ingredientePrincipal = args[0];
+		
+		switch( ingredientePrincipal ) {
+			case "tomate":
+				System.out.println("Picar tomate, sal y aceite.");
+				break;
+			case "huevo":
+				System.out.println("En una sarten poner un huevo a freir.");
+				break;
+			default:
+				System.out.println("Abrir lata atún, picar cebolla y limón.");
+		}
+		
+		
+	}
+
+}
